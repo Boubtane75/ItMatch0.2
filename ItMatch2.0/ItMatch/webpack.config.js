59830868268f0d30f5d0eca9625678d44adad0df
xWiZz <vcn.cvz@icloud.com>
2019-09-12 17:04:30 +0200

    front website

--- ItMatch2.0/ItMatch/webpack.config.js
+++ ItMatch2.0/ItMatch/webpack.config.js
@@ -24,6 +24,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    // .addEntry('login', './assets/js/login.js')
+    .addStyleEntry('login', './assets/css/login.css')
+    .addStyleEntry('register', './assets/css/register.css')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
